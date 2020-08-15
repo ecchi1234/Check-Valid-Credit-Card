26f641a73798e2fa5f95715a7f4cf5eb74c985e0
ecchi1234 <48946464+ecchi1234@users.noreply.github.com>
2020-08-15 22:25:21 +0700

    update

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 /**
- * program to check if credit card number is valid base on Luhn Algorithm
+ * program to check if credit card number is valid using Luhn Algorithm
  * 
  */
 // function: checkValidCard
-// @param: cardNumber 
+// @param: cardNumber : string
 // return: true or false
  
 function checkValidCard(cardNumber) {
@@ -24,18 +24,47 @@ function checkValidCard(cardNumber) {
     console.log(newArray);
     var checkSum = newArray.reduce(function (accumulator, current) {
         return accumulator + current;
-    })
+    });
     console.log(checkSum);
     return (checkSum % 10 === 0) ? true : false;
 
 }
 function main() {
     var cardNumber = document.getElementById("card-number").value;
+    console.log(cardNumber);
     if (checkValidCard(cardNumber)) {
         console.log(cardNumber);
         document.getElementById("result").innerText = "Valid card!";
     }
     else {
-        document.getElementById("result").innerText = "Invalid card!";
+        if (cardNumber === ''){
+            document.getElementById("result").innerText = "You haven't enter anything."; 
+        }
+        else{
+            document.getElementById("result").innerText = "Invalid card!";
+        }
     }
 }
+
+// test on console
+
+var test1 = [6, 0, 1, 1, 1, 4, 4, 3, 4, 0, 6, 8, 2, 9, 0, 5].join('');
+var test2 = [5, 7, 9, 5, 5, 9, 3, 3, 9, 2, 1, 3, 4, 6, 4, 3].join('');
+var test3 = [3, 7, 5, 7, 9, 6, 0, 8, 4, 4, 5, 9, 9, 1, 4].join('');
+console.log(test1);
+console.log(test2);
+console.log(test3);
+
+function test(cardNumber){
+    if (checkValidCard(cardNumber)){
+        console.log("valid");
+    }
+    else{
+        console.log("invalid");
+    }
+}
+test(test1);
+test(test2);
+test(test3);
+
+
